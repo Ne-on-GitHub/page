cb244bf68dd5453ab5bd10c5b7e72b7e23202512
Ne-on-GitHub <ne.on.the.score@gmail.com>
2024-02-27 13:24:57 +0900

    test 2

--- ovagod/ovagod.js
+++ ovagod/ovagod.js
@@ -1,12 +1,14 @@
 function setup(){
   createCanvas(windowWidth,windowHeight);
-  textSize(128);
   fill(255);
 }
 
 
 function draw(){
   background(64);
-  text("?",(sin(frameCount/100)*6+7)*width/15,(sin(frameCount/50)*2+4)*height/6.5);
+  textSize(sin(frameCount/5)*64+128);
+  translate((sin(frameCount/100)*6+7)*width/15,(sin(frameCount/50)*2+3)*height/6.5);
+  rotate(sin(frameCount/15)*3);
+  text("?",0,0);
 }
 function windowResized(){resizeCanvas(windowWidth,windowHeight)}
