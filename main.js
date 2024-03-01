99c7965a0f6106269a6ea7e6ad5fa05131a1b7ff
Ne-on-GitHub <ne.on.the.score@gmail.com>
2024-03-02 00:31:08 +0900

    Marged folder!

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
-let index;
+let page=0;
+let paged=65536;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   document.addEventListener("touchmove",scrollKill,{passive:false});
-  index=new anind();
-  index.setup();
+  //a.setup();
 }
 function draw(){
-  index.draw();
+  if(page==0){
+    if(paged!=0){
+      a.setup();
+      paged=0;}
+    a.draw();}
+  if(page==1){
+    if(paged!=1){
+      b.setup();
+      paged=1;}
+    b.draw();}
+  if(page==2){
+    if(paged!=2){
+      c.setup();
+      paged=2;}
+    c.draw();}
 }
 function windowResized() {
   resizeCanvas(windowWidth, windowHeighttextAscent()+textDescent());
@@ -17,7 +31,7 @@ function link(url, winName, options) {
 function scrollKill(e) {
   e.preventDefault();
 }
-class anind{
+class index{
   constructor(){
     this.w1;
     this.oosfeitwX=32;
@@ -31,6 +45,7 @@ class anind{
     this.backY=0;
     this.pmouseIsPressed;
     this.grf1;
+    this.frame;
   }
   setup(){
     textAlign(LEFT,TOP);
@@ -57,11 +72,14 @@ class anind{
     }
     grf1.updatePixels();
     this.grf1=grf1;
+    this.frame=0;
   }
   draw(){
     background(128,0,0);
     this.touchmove();
     this.oosfeitw();
+    this.densitydebug();
+    this.frame++;
   }
   touchmove(){
     let pmouseIsPressed=this.pmouseIsPressed;
@@ -104,14 +122,15 @@ class anind{
     let backX=this.backX;
     let backY=this.backY;
     let grf1=this.grf1;
+    let frame=this.frame;
     noFill();
     stroke(255);
     w1=textWidth("Only one symbol for everything in this world.");
-    let sin100ten=sin(frameCount/100)*10;
-    let sin200ten=sin(frameCount/200)*10;
+    let sin100ten=sin(frame/100)*10;
+    let sin200ten=sin(frame/200)*10;
     oosfeitwX=sin100ten+32;
-    oosfeitwY=sin(frameCount/50)*5+500;
-    if(PI*600<=frameCount%(PI*800)&&frameCount%(PI*800)<=PI*800){
+    oosfeitwY=sin(frame/50)*5+500;
+    if(PI*600<=frame%(PI*800)&&frame%(PI*800)<=PI*800){
       let a=random(-sin100ten,sin100ten);
       let b=random(-sin100ten,sin100ten);
       oosfeitwB.push(new oosfeitwBall(random(oosfeitwX,oosfeitwX+w1),random(oosfeitwY,oosfeitwY+textAscent()+textDescent()),random(-1,1),random(-1,1)));
@@ -139,7 +158,7 @@ class anind{
     if(pressed1==3)fill(0,32,86);
     if(pressed1==4){
       fill(255);
-      link("https://ne-on-github.github.io/page/ovagod/");}
+      page=1;}
     stroke(128);
     image(grf1,backX-w1/2+84,backY+444);
     text("Only one symbol for everything in this world.",boosfeitwX,boosfeitwY);
@@ -151,7 +170,32 @@ class anind{
     this.backY=backY;
     this.grf1=grf1;
   }
+  densitydebug(){
+    let bx=this.backX;
+    let by=this.backY;
+    stroke(0);
+    if(pixelDensity()==1){
+      line(bx+600,by+75,bx+620,by+220);
+    }
+    if(pixelDensity()==2){
+      line(bx+585,by+85,bx+630,by+65);
+      line(bx+615,by+70,bx+655,by+80);
+      line(bx+655,by+75,bx+660,by+90);
+      line(bx+665,by+85,bx+580,by+210);
+      line(bx+575,by+205,bx+685,by+215);
+      line(bx+680,by+210,bx+685,by+230);
+    }
+    if(pixelDensity()==3){
+      line(sq(random(20))*random(1,-1)+632+bx,sq(random(20))*random(1,-1)+150+by,sq(random(20))*random(1,-1)+632+bx,sq(random(20))*random(1,-1)+150+by);
+    }
+    if(pixelDensity()>3){
+      for(let n=0;n<16;n++){
+        line(sq(random(20))*random(1,-1)+632+bx,sq(random(20))*random(1,-1)+150+by,sq(random(20))*random(1,-1)+632+bx,sq(random(20))*random(1,-1)+150+by);
+      }
+    }
+  }
 }
+let a=new index();
 class oosfeitwBall{
   constructor(x,y,sx,sy){
     this.x=x;
@@ -171,3 +215,46 @@ class oosfeitwBall{
     this.life--;
   }
 }
+class ovagod{
+  constructor(){
+    this.a=64;
+    this.frame;
+  }
+  draw(){
+    let a=this.a;
+    let frame=this.frame;
+    background(64);
+    push();
+    fill(255);
+    textSize(sin(frame/5)*64+128);
+    translate((sin(frame/100)*6+7)*width/15,(sin(frame/50)*2+3)*height/6.5);
+    rotate(sin(frame/15)*3);
+    textAlign(LEFT,CENTER);
+    text("?",0,0);
+    textSize(16);
+    pop();
+    textAlign(CENTER,CENTER);
+    if(frame>1500){
+      fill(a,128);
+      if(a<128)a++;
+      noStroke();
+      text("Wow! This world is full of mysteries ;-)",width/2,height/2);
+    }
+    frame++;
+    this.a=a;
+    this.frame=frame;
+  }
+  setup(){
+    this.frame=0;
+  }
+}
+let b=new ovagod();
+class hsobahe{
+  constructor(){
+  }
+  setup(){
+  }
+  draw(){
+  }
+}
+let c=new hsobahe();
